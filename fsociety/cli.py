a3bc77baa82cfc855db3a731844c227d2d408cde
Aidan Holland <thehappydinoa@gmail.com>
2020-05-17 13:27:48 -0400

    feat: Colorama

--- fsociety/cli.py
+++ fsociety/cli.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# 3rd Party
+import colorama
+from colorama import Fore, Back, Style
+
 # Core
 from fsociety.menu import set_readline
 
@@ -9,17 +13,15 @@ import fsociety.information_gathering
 import fsociety.passwords
 
 # Menu
-MENU_TITLE = """
+MENU_TITLE = Fore.RED + """
 888888 .dP"Y8  dP"Yb   dP""b8 88 888888 888888 Yb  dP 
 88__   `Ybo." dP   Yb dP   `" 88 88__     88    YbdP  
 88""   o.`Y8b Yb   dP Yb      88 88""     88     8P   
 88     8bodP'  YbodP   YboodP 88 888888   88    dP    
 
-"""
+""" + Fore.RESET
 MENU_ITEMS = [fsociety.information_gathering, fsociety.passwords]
 items = dict()
-
-# Menu
 
 
 def command_name(module):
@@ -35,7 +37,7 @@ def menuitems():
     for value in MENU_ITEMS:
         name = command_name(value)
         tools = menutools(value)
-        items_str += f"{name}: \n{tools}\n\n"
+        items_str += f"{Back.WHITE + Fore.BLACK}{name}:{Style.RESET_ALL} \n{tools}\n\n"
     return items_str
 
 
@@ -48,6 +50,7 @@ def mainloop():
     print(menuitems())
     selected_command = input("Enter Command: ").strip()
     if not selected_command:
+        print("Invalid Command")
         return
     print()
     try:
@@ -57,8 +60,8 @@ def mainloop():
         print(str(e))
 
 
-# CLI
 def cli():
+    colorama.init()
     try:
         while True:
             set_readline(items.keys())
--- setup.py
+++ setup.py
@@ -20,7 +20,8 @@ VERSION = '3.0.0'
 
 # Required Packages
 REQUIRED = [
-    'requests'
+    'requests',
+    'colorama'
 ]
 
 # Optional Packages
@@ -41,6 +42,43 @@ except FileNotFoundError:
     long_description = DESCRIPTION
 
 
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system(
+            '{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(VERSION))
+        os.system('git push --tags')
+
+        sys.exit()
+
+
 setup(
     name=NAME,
     version=VERSION,
@@ -54,6 +92,9 @@ setup(
     py_modules=['fsociety'],
     entry_points={
         'console_scripts': ['fsociety=fsociety:cli'],
+    },
+    cmdclass={
+        'upload': UploadCommand,
     },
     install_requires=REQUIRED,
     extras_require=EXTRAS,
