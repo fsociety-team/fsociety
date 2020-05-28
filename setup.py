d3b8f24d59c36f33bf52994a9b720832640ac51a
Aidan Holland <thehappydinoa@gmail.com>
2020-05-28 14:42:30 -0400

    chore: Add python 3.8 support

--- setup.py
+++ setup.py
@@ -103,6 +103,7 @@ setup(
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: PyPy'
     ],
     # python setup.py upload
